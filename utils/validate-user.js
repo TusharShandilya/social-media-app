ab1b88d5fb67dcb68612ab6241eb5ea41e2f399d
Tushar Shandilya <tusharshandilya@live.com>
2020-12-22 16:46:06 +0530

    user validation utility created

---
+++ utils/validate-user.js
@@ -0,0 +1,93 @@
+const validateRegistrationInput = (
+  username,
+  email,
+  firstName,
+  lastName,
+  password,
+  confirmPassword
+) => {
+  let errors = {};
+
+  if (username.trim() === "") {
+    errors.username = "Username should not be empty";
+  }
+  if (email.trim() === "") {
+    errors.email = " Email should not be empty";
+  } else if (
+    !email.match(
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+    )
+  ) {
+    errors.email = "Please enter a valid email";
+  }
+  if (firstName.trim() === "") {
+    errors.firstName = "First name should not be empty";
+  }
+  if (lastName.trim() === "") {
+    errors.lastName = "Last name should not be empty";
+  }
+  errors = { ...errors, ...validatePasswordInput(password) };
+  if (confirmPassword.trim() === "") {
+    errors.confirmPassword = "Confirm password should not be empty";
+  } else if (confirmPassword === password) {
+    errors.confirmPassword = "Confirm password & password don't match";
+  }
+
+  return { errors, valid: Object.keys(errors).length < 1 };
+};
+
+const validateLoginInput = (username, password) => {
+  let errors = {};
+
+  if (username.trim() === "") {
+    errors.username = "Username should not be empty";
+  }
+  errors = { ...errors, ...validatePasswordInput(password) };
+  return { errors, valid: Object.keys(errors).length < 1 };
+};
+
+const validateUserEditInput = (firstName, lastname, email) => {
+  let errors = {};
+
+  if (email.trim() === "") {
+    errors.email = " Email should not be empty";
+  } else if (
+    !email.match(
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+    )
+  ) {
+    errors.email = "Please enter a valid email";
+  }
+  if (firstName.trim() === "") {
+    errors.firstName = "First name should not be empty";
+  }
+  if (lastName.trim() === "") {
+    errors.lastName = "Last name should not be empty";
+  }
+
+  return { errors, valid: Object.keys(errors).length < 1 };
+};
+
+const validatePasswordInput = (password) => {
+  if (password.trim() === "") {
+    return {
+      password: "Password should not be empty",
+    };
+  } else if (
+    password.match(/^(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.[!#$%^&? "])$/)
+  ) {
+    return {
+      password:
+        "Password should contain at least one digit\nPassword should contain at least one lower case\nPassword should contain at least one upper case\nPassword should contain at least 8 from the mentioned characters",
+    };
+  }
+
+  return {};
+};
+
+module.exports = {
+  validateLoginInput,
+  validateRegistrationInput,
+  validateUserEditInput,
+  validatePasswordInput,
+};
